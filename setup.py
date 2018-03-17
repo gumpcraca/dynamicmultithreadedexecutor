61ae2f65a131d49550a70fc9b4472859c73af6bc
Kevin McCabe <kmccabe2@cisco.com>
2018-03-16 20:13:15 -0400

    adding sentinels

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup, find_packages
 
+print("STARTING INSTALL!!!!")
+print(find_packages())
+
 setup(name='dynamicmultithreadedexecutor',
       version='1.0.2',
       description='Dynamic Multi-threaded Executor',
@@ -11,7 +14,7 @@ setup(name='dynamicmultithreadedexecutor',
       download_url="https://github.com/gumpcraca/dynamicmultithreadedexecutor/archive/1.0.2.zip",
       keywords = [],
       packages=find_packages(),
-      install_requires=['six'],
+      install_requires=['six','sentinels'],
       py_modules=["six"],
       classifiers = [],
      )
